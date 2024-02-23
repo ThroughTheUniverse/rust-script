b46b085dab1ba43857db800b3418b1eb420399c0
root <root@DESKTOP-I14DGAN.>
2024-02-23 22:07:23 +0800

    small change in debug.rs

--- src/chunk/debug.rs
+++ src/chunk/debug.rs
@@ -16,6 +16,104 @@ impl Chunk {
         }
     }
 
+    pub fn disassemble_instruction(&self, offset: usize) -> usize {
+        use crate::chunk::opcode::OpCode::*;
+        use crate::value::Value::Function;
+        use JumpDirection::*;
+
+        print!("{offset:04} ");
+
+        if offset > 0 && self.line_numbers[offset] == self.line_numbers[offset - 1] {
+            print!("   | ");
+        } else {
+            print!("{:4} ", self.line_numbers[offset]);
+        }
+
+        let instruction: OpCode = self.code[offset].into();
+        match instruction {
+            // constant
+            Constant => self.constant_instruction(Constant.to_string().as_str(), offset),
+            // literals
+            None => self.simple_instruction(None.to_string().as_str(), offset),
+            True => self.simple_instruction(True.to_string().as_str(), offset),
+            False => self.simple_instruction(False.to_string().as_str(), offset),
+            // global variable pop
+            Pop => self.simple_instruction(Pop.to_string().as_str(), offset),
+            // local variable
+            GetLocal => self.byte_instruction(GetLocal.to_string().as_str(), offset),
+            SetLocal => self.byte_instruction(SetLocal.to_string().as_str(), offset),
+            // global variable
+            GetGlobal => self.constant_instruction(GetGlobal.to_string().as_str(), offset),
+            SetGlobal => self.constant_instruction(SetGlobal.to_string().as_str(), offset),
+            DefineGlobal => self.constant_instruction(DefineGlobal.to_string().as_str(), offset),
+            // upvalue
+            GetUpvalue => self.byte_instruction(GetUpvalue.to_string().as_str(), offset),
+            SetUpvalue => self.byte_instruction(SetUpvalue.to_string().as_str(), offset),
+            CloseUpvalue => self.simple_instruction(CloseUpvalue.to_string().as_str(), offset),
+            // property
+            GetProperty => self.constant_instruction(GetProperty.to_string().as_str(), offset),
+            SetProperty => self.constant_instruction(SetProperty.to_string().as_str(), offset),
+            // super
+            GetSuper => self.constant_instruction(GetSuper.to_string().as_str(), offset),
+            // comparison
+            Equal => self.simple_instruction(Equal.to_string().as_str(), offset),
+            Greater => self.simple_instruction(Greater.to_string().as_str(), offset),
+            Less => self.simple_instruction(Less.to_string().as_str(), offset),
+            // binary
+            Add => self.simple_instruction(Add.to_string().as_str(), offset),
+            Subtract => self.simple_instruction(Subtract.to_string().as_str(), offset),
+            Multiply => self.simple_instruction(Multiply.to_string().as_str(), offset),
+            Divide => self.simple_instruction(Divide.to_string().as_str(), offset),
+            // not
+            Not => self.simple_instruction(Not.to_string().as_str(), offset),
+            // negate
+            Negate => self.simple_instruction(Negate.to_string().as_str(), offset),
+            // print
+            Print => self.simple_instruction(Print.to_string().as_str(), offset),
+            // jump
+            Jump => self.jump_instruction(Jump.to_string().as_str(), Forward, offset),
+            JumpIfFalse => self.jump_instruction(JumpIfFalse.to_string().as_str(), Forward, offset),
+            // loop
+            Loop => self.jump_instruction(Loop.to_string().as_str(), Backward, offset),
+            // invoke
+            Invoke => self.invoke_instruction(Invoke.to_string().as_str(), offset),
+            SuperInvoke => self.invoke_instruction(SuperInvoke.to_string().as_str(), offset),
+            // closure
+            Closure => {
+                let mut i = offset + 1;
+                let constant = self.code[i];
+                i += 1;
+                print!("{:-16} {constant:4} ", Closure.to_string());
+                self.constant_pool.print_nth(constant as usize);
+                println!();
+                if let Function(function) = self.constant_pool.get(constant as usize) {
+                    for _ in 0..function.upvalue_count {
+                        let is_local: &str = if self.code[i] == 0 {
+                            "upvalue"
+                        } else {
+                            "local"
+                        };
+                        i += 1;
+                        let index = self.code[i];
+                        i += 1;
+                        println!("{:04}      |                     {is_local} {index}", i - 2);
+                    }
+                } else {
+                    panic!("No function at position {constant}");
+                }
+                i
+            }
+            // function
+            Call => self.byte_instruction(Call.to_string().as_str(), offset),
+            Return => self.simple_instruction(Return.to_string().as_str(), offset),
+            // struct
+            Struct => self.constant_instruction(Struct.to_string().as_str(), offset),
+            Method => self.constant_instruction(Method.to_string().as_str(), offset),
+            Inherit => self.simple_instruction(Inherit.to_string().as_str(), offset),
+            _ => panic!("Unknown Opcode"),
+        }
+    }
+
     fn constant_instruction(&self, name: &str, offset: usize) -> usize {
         let index = self.code[offset + 1];
         print!("{name:<16} {index:>4} '");
@@ -53,84 +151,5 @@ impl Chunk {
         };
         println!("{name:<16} {offset:>4} -> {jump}");
         offset + 3
-    }
-
-    pub fn disassemble_instruction(&self, offset: usize) -> usize {
-        use crate::chunk::opcode::OpCode::*;
-        use crate::value::Value::Function;
-        use JumpDirection::*;
-
-        print!("{offset:04} ");
-
-        if offset > 0 && self.line_numbers[offset] == self.line_numbers[offset - 1] {
-            print!("   | ");
-        } else {
-            print!("{:4} ", self.line_numbers[offset]);
-        }
-
-        let instruction: OpCode = self.code[offset].into();
-        match instruction {
-            Constant => self.constant_instruction(Constant.to_string().as_str(), offset),
-            Return => self.simple_instruction(Return.to_string().as_str(), offset),
-            Negate => self.simple_instruction(Negate.to_string().as_str(), offset),
-            Add => self.simple_instruction(Add.to_string().as_str(), offset),
-            Subtract => self.simple_instruction(Subtract.to_string().as_str(), offset),
-            Multiply => self.simple_instruction(Multiply.to_string().as_str(), offset),
-            Divide => self.simple_instruction(Divide.to_string().as_str(), offset),
-            None => self.simple_instruction(None.to_string().as_str(), offset),
-            True => self.simple_instruction(True.to_string().as_str(), offset),
-            False => self.simple_instruction(False.to_string().as_str(), offset),
-            Not => self.simple_instruction(Not.to_string().as_str(), offset),
-            Equal => self.simple_instruction(Equal.to_string().as_str(), offset),
-            Greater => self.simple_instruction(Greater.to_string().as_str(), offset),
-            Less => self.simple_instruction(Less.to_string().as_str(), offset),
-            Print => self.simple_instruction(Print.to_string().as_str(), offset),
-            Pop => self.simple_instruction(Pop.to_string().as_str(), offset),
-            DefineGlobal => self.constant_instruction(DefineGlobal.to_string().as_str(), offset),
-            GetGlobal => self.constant_instruction(GetGlobal.to_string().as_str(), offset),
-            SetGlobal => self.constant_instruction(SetGlobal.to_string().as_str(), offset),
-            GetLocal => self.byte_instruction(GetLocal.to_string().as_str(), offset),
-            SetLocal => self.byte_instruction(SetLocal.to_string().as_str(), offset),
-            JumpIfFalse => self.jump_instruction(JumpIfFalse.to_string().as_str(), Forward, offset),
-            Jump => self.jump_instruction(Jump.to_string().as_str(), Forward, offset),
-            Loop => self.jump_instruction(Loop.to_string().as_str(), Backward, offset),
-            Call => self.byte_instruction(Call.to_string().as_str(), offset),
-            Closure => {
-                let mut i = offset + 1;
-                let constant = self.code[i];
-                i += 1;
-                print!("{:-16} {constant:4} ", Closure.to_string());
-                self.constant_pool.print_nth(constant as usize);
-                println!();
-                if let Function(function) = self.constant_pool.get(constant as usize) {
-                    for _ in 0..function.upvalue_count {
-                        let is_local: &str = if self.code[i] == 0 {
-                            "upvalue"
-                        } else {
-                            "local"
-                        };
-                        i += 1;
-                        let index = self.code[i];
-                        i += 1;
-                        println!("{:04}      |                     {is_local} {index}", i - 2);
-                    }
-                } else {
-                    panic!("No function at position {constant}");
-                }
-                i
-            }
-            GetUpvalue => self.byte_instruction(GetUpvalue.to_string().as_str(), offset),
-            SetUpvalue => self.byte_instruction(SetUpvalue.to_string().as_str(), offset),
-            CloseUpvalue => self.simple_instruction(CloseUpvalue.to_string().as_str(), offset),
-            Struct => self.constant_instruction(Struct.to_string().as_str(), offset),
-            GetProperty => self.constant_instruction(GetProperty.to_string().as_str(), offset),
-            SetProperty => self.constant_instruction(SetProperty.to_string().as_str(), offset),
-            Method => self.constant_instruction(Method.to_string().as_str(), offset),
-            Invoke => self.invoke_instruction(Invoke.to_string().as_str(), offset),
-            Inherit => self.simple_instruction(Inherit.to_string().as_str(), offset),
-            GetSuper => self.constant_instruction(GetSuper.to_string().as_str(), offset),
-            SuperInvoke => self.invoke_instruction(SuperInvoke.to_string().as_str(), offset),
-            _ => panic!("Unknown Opcode"),
-        }
     }
 }
